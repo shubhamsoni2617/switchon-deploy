13064fb08ea9b6db894faf7f696fecea90f99a0a
Shubham Soni <Shubham.soni@impactanalytics.co>
2022-01-15 22:33:13 +0530

    build

---
+++ client/build/precache-manifest.1146a37ef995dd96b3f29a42b1e589b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1f4b3396dc524216138ddd207b9baec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb43a3834064cdadc63e",
+    "url": "/static/css/main.facba66a.chunk.css"
+  },
+  {
+    "revision": "d21189e6a7cde67a74fd",
+    "url": "/static/js/2.c6950835.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.c6950835.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb43a3834064cdadc63e",
+    "url": "/static/js/main.5c64f7e2.chunk.js"
+  },
+  {
+    "revision": "c334d97a8d990f6458f9",
+    "url": "/static/js/runtime-main.5b226bcd.js"
+  }
+]);
