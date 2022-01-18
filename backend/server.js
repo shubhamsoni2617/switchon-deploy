a53e55a59325ed9f54e4d35f2abab94762631956
Shubham Soni <Shubham.soni@impactanalytics.co>
2022-01-18 22:26:29 +0530

    enabled cors

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,8 @@ const errorHandlers = require('./handlers/errorHandlers');
 const app = express();
 const port = process.env.PORT || 8000;
 app.use(express.json());
-// app.use(cors());
-// app.options('*', cors());
+app.use(cors());
+app.options('*', cors());
 
 app.use(require('./routes/user'));
 app.use(require('./routes/form'));
